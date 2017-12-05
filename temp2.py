45d506529c556ea3bc71a9ae91dcf87335e78ab6
nsv23 <mech.vamsi@gmail.com>
2017-12-05 16:03:15 +0100

    1. TrailAndError

--- temp2.py
+++ temp2.py
@@ -7,10 +7,10 @@ import numpy as np
 itr = 1000   # 198 full read
 reset_point = 198
 start = 0
-batch_size_counter = 251
-batch_size = 251
-sent_max_len = 40
-word_max_len = 25
+batch_size_counter = 5
+batch_size = 5
+sent_max_len = 10
+word_max_len = 15
 char_codes = 91
 char_embed_size = 50
 word_embed_size = 300
@@ -18,7 +18,7 @@ sent_embed_size = 600
 no_of_classes = 45
 learning_rate = 0.001
 
-# Placeholder
+# Placeholder (They work)
 char_id = tf.placeholder(dtype=tf.int32, shape=[None, word_max_len])
 word_id = tf.placeholder(dtype=tf.int32, shape=[None, sent_max_len])
 y = tf.placeholder(dtype=tf.int32, shape=[None, sent_max_len])
@@ -47,18 +47,20 @@ with tf.name_scope("WordLayer"):
         # print(output_sent)                                                                # shape=(12550, 600)
 
 with tf.name_scope("ForwardLayer"):
-    h_layer_weights = tf.Variable(tf.random_normal([sent_embed_size, no_of_classes]))
-    h_layer_bias = tf.Variable(tf.zeros([no_of_classes]))
+    h_layer_weights = tf.Variable(tf.random_normal([sent_embed_size, 4]))
+    h_layer_bias = tf.Variable(tf.zeros([4]))
     predicted_output = tf.matmul(output_sent, h_layer_weights) + h_layer_bias
-    print(predicted_output)                                                               # shape=(12550, 45)
+    # print(predicted_output)                                                               # shape=(12550, 45)
 
 with tf.name_scope("CostFunction"):
-    y_reshape = tf.reshape(y, [-1, 1])                                                      # (1200,1)*
-    # y_reshape = tf.reshape(y, [-1])                                                       # Tensor("CostFunction/Reshape:0", shape=(1200,), dtype=int32)
-    print(y_reshape)
+    # y_reshape = tf.reshape(y, [-1, 1])                                                  # (12550,1)*
+    y_reshape = tf.reshape(y, [-1])                                                       # Tensor("CostFunction/Reshape:0", shape=(12550,), dtype=int32)
+    yy =  tf.reshape(y, [-1, 1])
+    yyz = tf.reshape(y, [-1])
+    # print(y_reshape)
     weights = tf.cast(tf.where(y_reshape > 0, tf.ones_like(y_reshape), tf.zeros_like(y_reshape)), tf.float32)
-    y_reshape = tf.one_hot(y_reshape, depth=no_of_classes)
-    print(y_reshape)                                                                       # Tensor("CostFunction/one_hot:0", shape=(1200, 45), dtype=float32) # shape=(12550, 1, 45)*
+    y_reshape = tf.one_hot(y_reshape, depth=4)
+    # print(y_reshape)                                                                       # Tensor("CostFunction/one_hot:0", shape=(12550, 45), dtype=float32) # shape=(12550, 1, 45)*
     # y_reshape = tf.unstack(y_reshape, axis=1)
     # print(y_reshape)                                                                     # shape=(12550, 45)*
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=predicted_output, labels=y_reshape) * weights)
@@ -79,29 +81,38 @@ with tf.Session() as sess:
 
     char_id_batch, word_id_batch, pos_id_batch = temp1.retrieve_batch_sent(start, batch_size_counter, sent_max_len, word_max_len)
     feed_dict = {char_id: char_id_batch, word_id: word_id_batch, y: pos_id_batch}
-    for i in range(1, itr):
-        if i%reset_point != 0:
-            char_id_batch, word_id_batch, pos_id_batch = temp1.retrieve_batch_sent(start, batch_size_counter, sent_max_len, word_max_len)
-            feed_dict = {char_id: char_id_batch, word_id: word_id_batch, y: pos_id_batch}
-            # char_id_batch = np.ndarray(char_id_batch)
-            # print("\nprinting char ids")
-            # print(char_id_batch)
-            # print((np.shape(char_id_batch)))
-            # print("\nprinting word ids")
-            # print(word_id_batch)
-            # print((np.shape(word_id_batch)))
-            # print("\nprinting pos ids")
-            # print(pos_id_batch)
-            # print((np.shape(pos_id_batch)))
-            start = batch_size_counter
-            batch_size_counter = batch_size + batch_size_counter
-            _, ls = sess.run((train_op, loss), feed_dict=feed_dict)
-            print('Loss at iteration %d is: %.3f' % (i, ls))
-            # if (i % 10) == 0:
-            #     acc_result = sess.run(accuracy, feed_dict=feed_dict)
-            #     print('iteration: ', i, ' ', "Accuracy: ", i, ' - ', acc_result)
-        else:
-            # print("hello")
-            start = 0
-            batch_size_counter = 251
-            batch_size = 251
+    c, w, o, p = sess.run((yy, yyz, y_reshape, predicted_output), feed_dict=feed_dict)
+    print(c)
+    print('\n')
+    print(w)
+    print('\n')
+    print(o)
+    print('\n')
+    print(p)
+
+    # for i in range(1, itr):
+    #     if i%reset_point != 0:
+    #         char_id_batch, word_id_batch, pos_id_batch = temp1.retrieve_batch_sent(start, batch_size_counter, sent_max_len, word_max_len)
+    #         feed_dict = {char_id: char_id_batch, word_id: word_id_batch, y: pos_id_batch}
+    #         # char_id_batch = np.ndarray(char_id_batch)
+    #         # print("\nprinting char ids")
+    #         # print(char_id_batch)
+    #         # print((np.shape(char_id_batch)))
+    #         # print("\nprinting word ids")
+    #         # print(word_id_batch)
+    #         # print((np.shape(word_id_batch)))
+    #         # print("\nprinting pos ids")
+    #         # print(pos_id_batch)
+    #         # print((np.shape(pos_id_batch)))
+    #         start = batch_size_counter
+    #         batch_size_counter = batch_size + batch_size_counter
+    #         _, ls = sess.run((train_op, loss), feed_dict=feed_dict)
+    #         print('Loss at iteration %d is: %.3f' % (i, ls))
+    #         # if (i % 10) == 0:
+    #         #     acc_result = sess.run(accuracy, feed_dict=feed_dict)
+    #         #     print('iteration: ', i, ' ', "Accuracy: ", i, ' - ', acc_result)
+    #     else:
+    #         # print("hello")
+    #         start = 0
+    #         batch_size_counter = 251
+    #         batch_size = 251
